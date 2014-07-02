53ce9b14c9f8f03153ee1a9795a9d5b01b8b59a3
Shmo Chuang <elisajchuang@gmail.com>
2014-07-02 13:48:08 -0400

    adding clock.php

---
+++ clock.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(-1); # Report all PHP errors
+ini_set('display_errors', 1);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP Clock</title>
+	
+	<link href="styles.css" type="text/css" rel="stylesheet">
+	<?php require('logic.php'); ?>
+	
+</head>
+<body class="<?php echo $class ?>">
+	<h1>It is <?php echo $current_time ?> </h1>
+	<p>Time zone: <?php echo $time_zone ?> </p>
+	
+	<img href="http://making-the-internet.s3.amazonaws.com/<?php echo $image ?>"
+
+</body>
+</html>
